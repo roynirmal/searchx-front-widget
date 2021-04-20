e94c2f1c9a9080b1af59b94372409325a97784f0
David Maxwell <maxwelld90@gmail.com>
2021-04-20 15:50:58 +0100

    Working on page tracking configuration

--- src/js/app/search/results/SearchResultsContainer.js
+++ src/js/app/search/results/SearchResultsContainer.js
@@ -44,7 +44,6 @@ export default class SearchResultsContainer extends React.Component {
 
     componentWillMount() {
         SearchStore.addChangeListener(this._onChange);
-        console.log('search results container mount');
     }
 
     componentWillUnmount() {
